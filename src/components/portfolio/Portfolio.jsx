680dcc0106c07c9f89ac7103bcefa8b34d1d72d4
Khaled <kaljaaid@ucsc.edu>
2024-12-19 16:20:59 -0800

    updated order again

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,9 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import imageGenPhoto from "../../assets/images/imageGen.png";
 import facial_recognition from "../../assets/images/Facial_recognition.png";
 import FL from "../../assets/images/FL.png";
-import kvs from '../../assets/images/kvs.png';
+import kvs from '../../assets/images/kvs.png'
+
+
 
 const items = [
   {
@@ -12,12 +14,14 @@ const items = [
     title: "Federated Learning Platform",
     img: FL,
     desc: "Developed an open-source decentralized federated learning system using IBM's FL library and EdgeLake blockchain technology, earning an invitation to present the project to IBM's technical team and Linux Foundation",
+
   },
   {
     id: 2,
     title: "ImageGen",
     img: imageGenPhoto,
     desc: "Developed and integrated image generation models, specifically Stable Diusion.",
+
   },
   {
     id: 3,
@@ -35,6 +39,7 @@ const items = [
 
 const Single = ({ item }) => {
   const ref = useRef();
+
   const { scrollYProgress } = useScroll({
     target: ref,
   });
@@ -42,25 +47,22 @@ const Single = ({ item }) => {
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
-    <section>
+    <section >
       <div className="container">
         <div className="wrapper">
-          <div className="content-container">
-            <motion.div className="textContainer" style={{y}}>
-              <h2>{item.title}</h2>
-            </motion.div>
-            <div className="imageContainer" ref={ref}>
-              <img src={item.img} alt="" />
-            </div>
-            <motion.div className="textContainer description" style={{y}}>
-              <p>{item.desc}</p>
-              {item.id === 2 && (
-                <a href="https://imageeditv2.d1t6iofhrx2j14.amplifyapp.com/" target="_blank" rel="noopener noreferrer">
-                  <button>See Demo</button>
-                </a>
-              )}
-            </motion.div>
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
           </div>
+          <motion.div className="textContainer" style={{y}}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            {item.id === 2 && (
+              <a href="https://imageeditv2.d1t6iofhrx2j14.amplifyapp.com/" target="_blank" rel="noopener noreferrer">
+                <button>See Demo</button>
+              </a>
+            )}
+            {/* <button>See Demo</button> */}
+          </motion.div>
         </div>
       </div>
     </section>
@@ -69,6 +71,7 @@ const Single = ({ item }) => {
 
 const Portfolio = () => {
   const ref = useRef();
+
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["end end", "start start"],
