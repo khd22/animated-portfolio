9c2104d44b30777d9ebc712d6ac6ce09a6bf09a8
Khaled <kaljaaid@ucsc.edu>
2025-01-16 18:11:13 -0800

    an1

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,16 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
-import ReactGA from 'react-ga';
-const TRACKING_ID = "G-MHBPFJ0Q30"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+import React from 'react';
+import App from './App';
+import ReactGA from 'react-ga4';
+
+
+ReactGA.initialize('G-MHBPFJ0Q30'); // Replace with your actual Measurement ID
+
 const App = () => {
+  ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+
   return (
     <div>
       {/* <Cursor /> */}
